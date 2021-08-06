7480dfeaf6504dcd5d9bc825cabbe4703e1ac08f
Asabeneh <asabeneh@gmail.com>
2021-08-06 22:15:16 +0300

    scrapping script has been added

---
+++ web-scrapping.py
@@ -0,0 +1,35 @@
+# The internet is full data and information and it is possible to scrap data from websites using Python
+# Let's scrap USA Presidents name from wikipedia using this link (https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States)
+
+# importing important packages
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+# US presidents list url
+
+url = 'https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States'
+
+response = requests.get(url)
+content = response.content
+
+
+soup = BeautifulSoup(content, 'html.parser')
+title = soup.title.get_text()
+print(title)
+
+table = soup.find('table', {'class': 'wikitable'})
+rows = table.find_all('tr')[1:]
+
+presidents = []
+for row in rows:
+    cells = row.find_all('td')
+    for i, cell in enumerate(cells):
+        if cell.find('b'):
+           president = cell.find('b').find('a').text
+           presidents.append(president)
+           
+pprint(presidents)
+
+
+
